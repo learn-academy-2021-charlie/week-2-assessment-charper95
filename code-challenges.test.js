adfe9dccb60473596cd14225cb55caca90695327
charper95 <charper1995@gmail.com>
2021-07-16 17:06:06 -0700

    finished code challenge problem 1

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,7 +1,7 @@
 // ASSESSMENT 2: Coding practical questions with Jest
 
-const { expect } = require("@jest/globals")
-const { describe } = require("yargs")
+// const { expect } = require("@jest/globals")
+// const { describe } = require("yargs")
 
 // Please read all questions thoroughly
 // All problems can be solved with concepts covered in class or in the syllabus
@@ -19,6 +19,47 @@ const { describe } = require("yargs")
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
+// a) Create a test with expect statements for each of the variables provided.
+
+// Describe the name of the function - divThree
+describe("divThree", () => {
+// Test what the function does - decides if a given number is evenly divisble by three or not
+    it("decides if a given number is evenly divisble by three or not", () => {
+// Expect the output of the function to be equal to the correct output - output of divThree to equal `${number} is divisible by three` or `${number} is not divisible by three`
+        expect(divThree(num1)).toEqual("15 is divisible by three")
+        expect(divThree(num2)).toEqual("0 is divisible by three")
+        expect(divThree(num3)).toEqual("-7 is not divisible by three")
+    })
+})
+
+// console.log test function
+// // Create a function called divThree
+// // That takes a number as an argument for the input
+// const divThree = (number) => {
+// // On the condition if the number is evenly divisble by three
+//     if (number % 3 === 0){
+// // Return an interpolated string "number is divisible by three"
+//         return `${number} is divisible by three`
+//     } else {
+// // or not
+//         return `${number} is not divisible by three`
+//     }
+// }
+
+var num1 = 15
+// console.log(divThree(num1))
+// Expected output: "15 is divisible by three"
+
+var num2 = 0
+// console.log(divThree(num2))
+// Expected output: "0 is divisible by three"
+
+var num3 = -7
+// console.log(divThree(num3))
+// Expected output: "-7 is not divisible by three"
+
+// b) Create the function that makes the test pass.
+
 // Create a function called divThree
 // That takes a number as an argument for the input
 const divThree = (number) => {
@@ -31,34 +72,6 @@ const divThree = (number) => {
         return `${number} is not divisible by three`
     }
 }
-
-// a) Create a test with expect statements for each of the variables provided.
-
-// Describe the name of the function - divThree
-describe("divThree", () => {
-// Test what the function does - decides if a given number is evenly divisble by three or not
-    it("decides if a given number is evenly divisble by three or not", () => {
-// Expect the output of the function to be equal to the correct output - output of divThree to equal `${number} is divisible by three` or `${number} is not divisible by three`
-        expect(divThree(true)).toEqual(`${number} is divisible by three`)
-        expect(divThree(false)).toEqual(`${number} is not divisible by three`)
-    }
-}
-
-
-var num1 = 15
-// Expected output: "15 is divisible by three"
-
-var num2 = 0
-// Expected output: "0 is divisible by three"
-
-var num3 = -7
-// Expected output: "-7 is not divisible by three"
-
-
-
-// b) Create the function that makes the test pass.
-
-
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
